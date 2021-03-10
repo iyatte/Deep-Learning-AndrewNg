2f141b59316adb75a6bf010b15f27d4dbe8d28a5
iyatte <1484711109@qq.com>
2021-03-10 22:00:37 +0000

    begin Course 2

---
+++ Course2/Week1 test/2_1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/3/10 21:59
+@Author  : yany
+@File    : 2.py
+"""
+
+if __name__ == "__main__":
+    pass
---
+++ Course2/Week1 test/README.md
@@ -0,0 +1,5 @@
+# Course 1-Week 2
+![1 icon](./raw_data/1.jpg)
+![2 icon](./raw_data/2.jpg)
+
+
